e09b02fbffcc484406d4368fd18c957b6ab411a8
Jackson Meade <jacksonmeade@outlook.com>
2019-06-25 00:55:03 -0400

    key definition

--- assets/hub/manager.js
+++ assets/hub/manager.js
@@ -1,5 +1,6 @@
+var key;
 
 function initializeConnection() {
-    var key = document.getElementById("ConferenceHub").contentWindow.document.getElementById("Movie").contentWindow;
+    key = document.getElementById("ConferenceHub").contentWindow.document.getElementById("Movie").contentWindow;
     key.set_conferenceStatus("inactive");
 }
