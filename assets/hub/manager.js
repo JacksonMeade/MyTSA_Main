6662034caa422d19ce1cc27305c1570fe5eefaba
Jackson Meade <jacksonmeade@outlook.com>
2019-06-25 00:32:07 -0400

    Separate JS

---
+++ assets/hub/manager.js
@@ -0,0 +1,5 @@
+
+function initializeConnection() {
+    var key = document.getElementById("ConferenceHub").contentWindow.document.getElementById("Movie").contentWindow;
+    key.set_conferenceStatus("inactive");
+}
