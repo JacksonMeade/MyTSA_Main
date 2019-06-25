f5bc7f695a2e3eac557db5eddb046cd356f897bc
Jackson Meade <jacksonmeade@outlook.com>
2019-06-25 18:29:09 -0400

    Visual input for internet connection loss

--- assets/hub/manager.js
+++ assets/hub/manager.js
@@ -1,8 +1,19 @@
 var key;
 
+overlay = document.getElementById("HoldPending").style
+overlay.display = "none";
+
+setInterval(UIStatus, 5000);
+
 function initializeConnection() {
     key = document.getElementById("ConferenceHub").contentWindow.document.getElementById("Movie").contentWindow;
     key.set_conferenceStatus("inactive");
+}
+
+function UIStatus() {
+    if (key.conferenceStatus === "Pending") {
+        overlay.display = "inherit";
+    }
 }
 
 function askStatus() {
