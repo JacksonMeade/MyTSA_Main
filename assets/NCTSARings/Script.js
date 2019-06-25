d3b8e53996021fc8c7caf08b4c5495e017e1ef1f
Jackson Meade <jacksonmeade@outlook.com>
2019-06-24 20:40:04 -0400

    alme?

--- assets/NCTSARings/Script.js
+++ assets/NCTSARings/Script.js
@@ -1,22 +1,7 @@
-function displayMessage (evt) {
+function alme(message) {
 StackTrace="General";
 
-	var message;
-	if (evt.origin !== "https://jacksonmeade.github.io/") {
-		message = "You are not worthy";
-	}
-	else {
-		message = "I got " + evt.data + " from " + evt.origin;
-	}
-	TextBoxSet("StatusText",message);
-}
-
-if (window.addEventListener) {
-	// For standards-compliant web browsers
-	window.addEventListener("message", displayMessage, false);
-}
-else {
-	window.attachEvent("onmessage", displayMessage);
+TextBoxSet("StatusText",message);
 
 }
 
