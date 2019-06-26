6e3dfbaeb4765ecb3b34e309f876ae39736d6659
Jackson Meade <jacksonmeade@outlook.com>
2019-06-25 23:53:26 -0400

    I don't want to disable standard motion

--- assets/custom/js/better-swipe.js
+++ assets/custom/js/better-swipe.js
@@ -26,7 +26,7 @@ var swipeDirection = null;
 
 function touchStart(event, passedName) {
     // disable the standard ability to select the touched object
-    event.preventDefault();
+    //event.preventDefault();
     // get the total number of fingers touching the screen
     fingerCount = event.touches.length;
     // since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
@@ -44,7 +44,7 @@ function touchStart(event, passedName) {
 }
 
 function touchMove(event) {
-    event.preventDefault();
+    //event.preventDefault();
     if (event.touches.length === 1) {
         curX = event.touches[0].pageX;
         curY = event.touches[0].pageY;
@@ -54,7 +54,7 @@ function touchMove(event) {
 }
 
 function touchEnd(event) {
-    event.preventDefault();
+    //event.preventDefault();
     // check to see if more than one finger was used and that there is an ending coordinate
     if (fingerCount === 1 && curX !== 0) {
         // use the Distance Formula to determine the length of the swipe
