28b32065fe4a48c76a64fc7902911623d761f44c
Jackson Meade <jacksonmeade@outlook.com>
2019-06-25 23:01:01 -0400

    Grow and shrink works cross-device

--- assets/custom/js/theme_ext.js
+++ assets/custom/js/theme_ext.js
@@ -3,13 +3,65 @@ var sidebar;
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function () { scrollFunction(); };
 
-function changeSidebar() {
-        sidebar = document.getElementById("sidenav").style;
-    if (sidebar.transform === "initial") {
-        sidebar.transform = "translate(-95vw)";
+function blinds(id) {
+    if (document.getElementById(("dropdown-" + id)).style.display === "none") {
+        document.getElementById(("dropdown-" + id)).style.display = "inherit";
+        document.getElementById(("arrow-" + id)).style.transform = "rotate(180deg)";
+
     }
     else {
-        sidebar.transform = "initial";
+        document.getElementById(("dropdown-" + id)).style.display = "none";
+        document.getElementById(("arrow-" + id)).style.transform = "rotate(0deg)";
+    }
+}
+
+function changeSidebar() {
+    if ($(window).width() > 768) {
+        // do something for big screens
+        if (!document.getElementById("sm-slide")) {
+            document.getElementById("sidenav").id = "sm-slide";
+            document.getElementById("md-slide").id = "sidenav";
+        }
+    }
+    else {
+        // do something for small screens
+        if (!document.getElementById("md-slide")) {
+            document.getElementById("sidenav").id = "md-slide";
+            document.getElementById("sm-slide").id = "sidenav";
+        }
+    }
+
+    sidebar = document.getElementById("sidenav");
+
+    if (sidebar.style.transform === "initial") {
+        var fullstring= String("transition:0.2s;transform:translate(-"+String(document.getElementsByClassName('sidebar')[0].offsetWidth)+"px);");
+        //if ($(window).width() < 768) {
+        //    // do something for small screens
+        //    sidebar.transform = "translate(-100vw)";
+        //}
+        //else if ($(window).width() >= 768 && $(window).width() <= 992) {
+        //    // do something for medium screens
+        //    sidebar.setAttribute("style",fullstring);
+        //}
+        //else if ($(window).width() > 992) {
+        //    // do something for big screens
+        //    sidebar.setAttribute("style", fullstring);
+        //}
+        sidebar.setAttribute("style", fullstring);
+
+        if ($(window).width() > 768) {
+            fullstring = String("width:100vw;transition:0.2s;transform:translate(" + String(document.getElementsByClassName('sidebar')[0].offsetWidth) + "px);");
+
+            document.getElementsByClassName('main')[0].setAttribute("class", "col-12 main");
+
+            document.getElementsByClassName('main')[0].setAttribute("style", fullstring);
+        }
+
+    }
+    else {
+        document.getElementsByClassName('main')[0].setAttribute("style", "");
+        document.getElementsByClassName('main')[0].setAttribute("class", "offset-lg-3 col-lg-9 offset-md-4 col-md-8 col-sm-12 main");
+        sidebar.setAttribute("style", "transition:0.2s;transform:initial");
     }
 }
 
