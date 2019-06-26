676f38d7c44eb2884cf6e5d567b5859700dbb202
Jackson Meade <jacksonmeade@outlook.com>
2019-06-26 00:14:22 -0400

    be tactical

--- assets/custom/js/better-swipe.js
+++ assets/custom/js/better-swipe.js
@@ -44,7 +44,7 @@ function touchStart(event, passedName) {
 }
 
 function touchMove(event) {
-    event.preventDefault();
+    //event.preventDefault();
     if (event.touches.length === 1) {
         curX = event.touches[0].pageX;
         curY = event.touches[0].pageY;
@@ -102,10 +102,13 @@ function caluculateAngle() {
 function determineSwipeDirection() {
     if ((swipeAngle <= 45) && (swipeAngle >= 0)) {
         swipeDirection = 'left';
+        event.preventDefault();
     } else if ((swipeAngle <= 360) && (swipeAngle >= 315)) {
         swipeDirection = 'left';
+        event.preventDefault();
     } else if ((swipeAngle >= 135) && (swipeAngle <= 225)) {
         swipeDirection = 'right';
+        event.preventDefault();
     } else if ((swipeAngle > 45) && (swipeAngle < 135)) {
         swipeDirection = 'down';
     } else {
